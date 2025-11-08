e5f3e861a4519b3cd1ea953b99d0a81951cbca1e
lucassing <singlucasmartin@gmail.com>
2025-11-08 22:43:48 +0100

    chore(frontend): load Bootstrap CSS and bundle JS in entrypoint

--- loan-frontend/src/main.tsx
+++ loan-frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import Routes from "@/routes";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
